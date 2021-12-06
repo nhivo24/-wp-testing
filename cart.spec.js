cf814a5b7592b93dbb7b07bc9d92f1832af4ab5f
nhivo24 <53256318+nhivo24@users.noreply.github.com>
2021-12-06 17:13:44 +0700

    format

--- cart.spec.js
+++ cart.spec.js
@@ -1,4 +1,5 @@
 describe('Shopping Cart Test Quantity Update', ()=>{
+    
     it('Verify the total amount after the quantity updated', ()=>{
        
         cy.visit("https://noithatquangmai.com/")  //Launch Browser & Open Url
@@ -14,18 +15,17 @@ describe('Shopping Cart Test Quantity Update', ()=>{
         cy.get(".price-wrapper > p > span.woocommerce-Price-amount").contains("1.224.000") //Verify the unit price 1.224.000đ
 
         cy.get(".price-wrapper").should('contain', '1.224.000')
-
         
         cy.get("#quantity_61aaa7aec16fd").clear().type('2')//Enter Quantity 2
-
         
         cy.get(".update-cart-button[name=updatecart]").click() //Update shopping cart
 
-     
         cy.get(".value-summary > strong").contains('2.448.000') //Verify Total  $2.448.000
+        
         cy.get(".value-summary > strong")
         .should(($total)=>{
         expect($total).to.contain('2.448.000')
+            
           })
     })
 })
